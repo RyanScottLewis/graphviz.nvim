3df7879d1d219a02ac8cc293babd8a86315a1917
izocha <isaias.carrillo@outlook.es>
2024-12-18 23:18:14 -0500

    Changes: - Initialization are now on ftplugin/lua.lua(on building)

---
+++ after/ftplugin/dot.lua
@@ -0,0 +1,25 @@
+if vim.g.graphviz_nvim_loaded then
+	-- require("graphviz").setup({})
+end
+
+vim.g.graphviz_nvim_loaded = true
+
+vim.api.nvim_buf_create_user_command(0, "GraphExport", function(opts)
+	local settings = require("graphviz").config
+	local args = opts.fargs[1] or settings.format
+	require("graphviz.commands").graph_export(args)
+end, {
+	desc = "Export graph",
+	--NOTE:-- Why "*"?, because this allow me to use default values
+	nargs = "*",
+	complete = function()
+		return require("graphviz.utils").args_complete()
+	end,
+})
+
+vim.api.nvim_buf_create_user_command(0, "GraphPreview", function()
+	local settings = require("graphviz").config
+	require("graphviz.commands").graph_preview(settings.preview)
+end, {
+	desc = "Graphviz preview on save",
+})
