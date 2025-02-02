edafa9574549eb3e0c084be0eb3dc69c498c90ad
izocha <isaias.carrillo@outlook.es>
2025-02-01 21:35:41 -0500

    feat(dot.lua): Plugin loading optimization (lazyload?)
    The plugin will now only load the modules and keymaps in dot files automatically.

--- after/ftplugin/dot.lua
+++ after/ftplugin/dot.lua
@@ -1,25 +1,24 @@
-if vim.g.graphviz_nvim_loaded then
+if not vim.g.graphviz_nvim_loaded then
 	-- require("graphviz").setup({})
+	vim.api.nvim_buf_create_user_command(0, "GraphExport", function(opts)
+		local settings = require("graphviz").config
+		local args = opts.fargs[1] or settings.format
+		require("graphviz.commands").graph_export(args)
+	end, {
+		desc = "Export graph",
+		--NOTE:-- Why "*"?, because this allow me to use default values
+		nargs = "*",
+		complete = function()
+			return require("graphviz.utils").args_complete()
+		end,
+	})
+
+	vim.api.nvim_buf_create_user_command(0, "GraphPreview", function()
+		local settings = require("graphviz").config
+		require("graphviz.commands").graph_preview(settings.preview)
+	end, {
+		desc = "Graphviz preview on save",
+	})
 end
 
 vim.g.graphviz_nvim_loaded = true
-
-vim.api.nvim_buf_create_user_command(0, "GraphExport", function(opts)
-	local settings = require("graphviz").config
-	local args = opts.fargs[1] or settings.format
-	require("graphviz.commands").graph_export(args)
-end, {
-	desc = "Export graph",
-	--NOTE:-- Why "*"?, because this allow me to use default values
-	nargs = "*",
-	complete = function()
-		return require("graphviz.utils").args_complete()
-	end,
-})
-
-vim.api.nvim_buf_create_user_command(0, "GraphPreview", function()
-	local settings = require("graphviz").config
-	require("graphviz.commands").graph_preview(settings.preview)
-end, {
-	desc = "Graphviz preview on save",
-})
